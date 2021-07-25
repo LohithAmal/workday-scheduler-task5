99965b4dd09b48fd56c3bc2b165ff410b5011f78
lohith <lohith05amal@gmail.com>
2021-07-25 04:02:06 -0400

    add logic pseudo code to js

---
+++ script.js
@@ -0,0 +1,22 @@
+// moment the current time
+
+// currenttime to the nearest time
+
+// start day time 9 am
+
+// Time blocks code here
+
+// 9 AM - 5AM- time formula link to html
+
+
+// call function for all workday hours with current time 
+// cordinating numbers
+
+// Add time1-time 9 and if else conditions
+
+
+// Loops through input area to get item from local storage
+
+// for loop gets data from local storage
+
+// save to local storage code
